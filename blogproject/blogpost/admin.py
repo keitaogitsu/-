0456b639d0cbd73b4aeb6556530d2593d2d92a0e
keitaogitsu <ogitsukeita@gmail.com>
2022-04-11 17:58:16 +0900

    dakokuprojのみにする

--- blogproject/blogpost/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SampleModel, BlogModel
-
-# Register your models here.
-
-admin.site.register(SampleModel)
-admin.site.register(BlogModel)
--- helloworldproject/helloworldapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def helloworldfunc(request):
-    return HttpResponse('hello world')
-
-# Create your views here.
